5048ff9af617bed7cb4591627092db230d7b216a
msegreto <msegreto1202@gmail.com>
2025-07-03 09:36:20 +0200

    HACSystem debugging 12

--- coap-network/humidity/coap/res_prediction.c
+++ coap-network/humidity/coap/res_prediction.c
@@ -1,11 +1,15 @@
 // coap/res_prediction.c
 #include "res_prediction.h"
-
 #include "sys/log.h"
+#include "coap-observe.h"
+#include "net/ipv6/uip.h"
+#include <stdio.h>
+
 #define LOG_MODULE "res_prediction"
 #define LOG_LEVEL LOG_LEVEL_APP
 
 static float last_prediction = 0;
+
 static void res_get_handler(coap_message_t *request, coap_message_t *response,
                             uint8_t *buffer, uint16_t preferred_size, int32_t *offset);
 static void res_event_handler(void);
@@ -19,12 +23,24 @@ EVENT_RESOURCE(res_prediction,
 void trigger_prediction_event() {
     last_prediction = 1;
     LOG_INFO("[Prediction] Triggering prediction event, value: %.2f\n", last_prediction);
-    // last_prediction = predict_humidity();
     res_prediction.trigger();
 }
 
 static void res_event_handler(void) {
     LOG_INFO("[Prediction] Notifying observers...\n");
+
+    coap_observee_t *o = coap_observees;
+    if (o == NULL) {
+        LOG_WARN("[Prediction] No observers found.\n");
+    }
+
+    while (o != NULL) {
+        LOG_INFO("Observer: ");
+        LOG_INFO_6ADDR(&o->addr);
+        LOG_INFO_(" port %u\n", uip_ntohs(o->port));
+        o = o->next;
+    }
+
     coap_notify_observers(&res_prediction);
 }
 
@@ -35,4 +51,3 @@ static void res_get_handler(coap_message_t *request, coap_message_t *response,
     LOG_INFO("[Prediction] GET request received, sending: %.2f\n", last_prediction);
     coap_set_payload(response, buffer, len);
 }
-
