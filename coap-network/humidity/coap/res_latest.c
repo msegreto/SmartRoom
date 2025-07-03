39f5bc71887a413df3cb5616bc340a77613c490a
msegreto <msegreto1202@gmail.com>
2025-07-03 09:21:31 +0200

    HACSystem debugging 10

--- coap-network/humidity/coap/res_latest.c
+++ coap-network/humidity/coap/res_latest.c
@@ -1,8 +1,11 @@
-// coap/res_latest.c
 #include "res_latest.h"
 #include "coap-constants.h"
 #include "../sensor/buffer.h"
 #include <stdio.h>
+
+#include "sys/log.h"
+#define LOG_MODULE "res_latest"
+#define LOG_LEVEL LOG_LEVEL_APP
 
 static void res_get_handler(coap_message_t *request, coap_message_t *response,
                             uint8_t *buffer, uint16_t preferred_size, int32_t *offset);
@@ -20,15 +23,16 @@ static void res_get_handler(coap_message_t *request, coap_message_t *response,
         coap_set_status_code(response, INTERNAL_SERVER_ERROR_5_00);
         return;
     }
-    
+
     float latest = get_latest_value();
     int len = snprintf((char *)buffer, preferred_size, "%.2f", latest);
-    
+
     if (len < 0 || len >= preferred_size) {
         coap_set_status_code(response, INTERNAL_SERVER_ERROR_5_00);
         return;
     }
-    
+
+    LOG_INFO("[Latest] GET request received, sending: %.2f\n", latest);
     coap_set_payload(response, buffer, len);
 }
 
@@ -37,5 +41,7 @@ void trigger_latest_event() {
 }
 
 static void res_event_handler(void) {
-    coap_notify_observers(&res_latest);
+    LOG_INFO("[Latest] Notifying observers...\n");
+    int notified = coap_notify_observers(&res_latest);
+    LOG_INFO("[Latest] Notification result: %d\n", notified);
 }
--- coap-network/humidity/coap/res_prediction.c
+++ coap-network/humidity/coap/res_prediction.c
@@ -1,6 +1,10 @@
 // coap/res_prediction.c
 #include "res_prediction.h"
 
+#include "sys/log.h"
+#define LOG_MODULE "res_prediction"
+#define LOG_LEVEL LOG_LEVEL_APP
+
 static float last_prediction = 0;
 static void res_get_handler(coap_message_t *request, coap_message_t *response,
                             uint8_t *buffer, uint16_t preferred_size, int32_t *offset);
@@ -14,17 +18,22 @@ EVENT_RESOURCE(res_prediction,
 
 void trigger_prediction_event() {
     last_prediction = 1;
+    LOG_INFO("[Prediction] Triggering prediction event, value: %.2f\n", last_prediction);
     // last_prediction = predict_humidity();
     res_prediction.trigger();
 }
 
 static void res_event_handler(void) {
-    coap_notify_observers(&res_prediction);
+    LOG_INFO("[Prediction] Notifying observers...\n");
+    int notified = coap_notify_observers(&res_prediction);
+    LOG_INFO("[Prediction] Notification result: %d\n", notified);
 }
 
 static void res_get_handler(coap_message_t *request, coap_message_t *response,
                             uint8_t *buffer, uint16_t preferred_size, int32_t *offset) {
-    last_prediction = 1; 
+    last_prediction = 1;
     int len = snprintf((char *)buffer, preferred_size, "%.2f", last_prediction);
+    LOG_INFO("[Prediction] GET request received, sending: %.2f\n", last_prediction);
     coap_set_payload(response, buffer, len);
 }
+
