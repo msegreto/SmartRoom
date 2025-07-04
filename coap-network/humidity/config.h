62e6c498f04b78e51991ee4de632e9f8534ac93b
msegreto <msegreto1202@gmail.com>
2025-07-04 16:06:40 +0200

    merge fixes2

--- coap-network/humidity/config.h
+++ coap-network/humidity/config.h
@@ -1,11 +1,11 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-#define SENSING_PERIOD_SECONDS 60
+#define SENSING_PERIOD_SECONDS 5
 #define MEAN_HUMIDITY 41.1
 #define STD_HUMIDITY 5.83
 #define MAX_STEP 2.00
-#define BUFFER_SIZE 60
+#define BUFFER_SIZE 10
 
 #define CLOUD_SERVER_EP "coap://[fd00::1]:5683"
 #define REGISTRATION_RESOURCE_PATH "registration"
