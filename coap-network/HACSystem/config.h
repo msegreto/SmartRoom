685a7f4c4981b188dc5bbb654552d0f05b82c777
msegreto <msegreto1202@gmail.com>
2025-07-05 10:53:33 +0200

    coap adaptation actuator 18

--- coap-network/HACSystem/config.h
+++ coap-network/HACSystem/config.h
@@ -7,8 +7,8 @@
 #define REGISTRATION_ACK_CODE CREATED_2_01
 #define MAX_REGISTRATION_RETRY 3
 #define REGISTRATION_WAIT_SECONDS 5
-#define OBS_HUM_RESOURCE_PATH  "service?resource=predh"
-#define OBS_TEMP_RESOURCE_PATH "service?resource=predt"
+#define OBS_HUM_URI  "coap://[fd00::1]:5683/service?resource=predh"
+#define OBS_TEMP_URI "coap://[fd00::1]:5683/service?resource=predt"
 
 
 #define DEFAULT_THRESHOLD_MIN 20.0
