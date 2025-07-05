8341b09e2040dd916e64a8e49f93418bdeb2b419
msegreto <msegreto1202@gmail.com>
2025-07-05 16:35:13 +0200

    coap adaptation actuator 42

--- coap-network/HACSystem/res-control.c
+++ coap-network/HACSystem/res-control.c
@@ -10,7 +10,6 @@
 #define LOG_LEVEL LOG_LEVEL_APP
 #include "sys/log.h"
 
-static float status = 0;
 static void res_get_handler(coap_message_t *request, coap_message_t *response,
                             uint8_t *buffer, uint16_t preferred_size, int32_t *offset);
 static void res_event_handler(void);
