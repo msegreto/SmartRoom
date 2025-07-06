02a6097d07a57b7372046d78838a2fcb24e7acc6
msegreto <msegreto1202@gmail.com>
2025-07-06 11:28:47 +0200

    added Node Shutdown & Restart9

--- coap-network/humidity/sensor/sensing.h
+++ coap-network/humidity/sensor/sensing.h
@@ -7,15 +7,6 @@
 #include <stdlib.h>
 #include <math.h>
 
-// Attiva il sensore
-void sensor_on(void);
-
-// Disattiva il sensore
-void sensor_off(void);
-
-// Restituisce 1 se il sensore è attivo, 0 altrimenti
-int sensor_is_active(void);
-
 // Genera una temperatura casuale con media e deviazione standard configurabili
 float generate_random_humidity(void);
 
