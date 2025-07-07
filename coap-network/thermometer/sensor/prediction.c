4c8ffb7e347174c505048c0216bf369341841f60
msegreto <msegreto1202@gmail.com>
2025-07-07 14:26:22 +0200

    starting ML integration 4

--- coap-network/thermometer/sensor/prediction.c
+++ coap-network/thermometer/sensor/prediction.c
@@ -1,21 +1,21 @@
 #include "prediction.h"
 #include "temperature_forecast_converted.h"
-#include "eml_trees.h"
 #include "buffer.h"
 #include <stdint.h>
 
 float predict_temperature(void) {
     float *buffer = get_buffer();
     int start = get_buffer_start_index();
-    int32_t input_fixed[BUFFER_SIZE];
+    int16_t input_fixed[BUFFER_SIZE];
 
     for (int i = 0; i < BUFFER_SIZE; i++) {
         int index = (start + i) % BUFFER_SIZE;
-        input_fixed[i] = (int32_t)((buffer[index] - MIN_INPUT) * SCALE);
+        input_fixed[i] = (int16_t)((buffer[index] - MIN_INPUT) * SCALE);
     }
 
-    int32_t prediction = eml_trees_regress(&temperature_forecast, input_fixed, BUFFER_SIZE);
-    float output = ((float)prediction / SCALE);
+    // Uso della funzione generata da emlearn per DecisionTreeRegressor
+    int32_t prediction = temperature_forecast_predict(input_fixed, BUFFER_SIZE);
 
+    float output = ((float)prediction / SCALE);
     return output;
 }
