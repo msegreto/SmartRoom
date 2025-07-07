c9baac99ec6d99a3734ada8852942895ba072ddc
msegreto <msegreto1202@gmail.com>
2025-07-07 13:56:55 +0200

    starting ML integration

--- coap-network/thermometer/sensor/prediction.c
+++ coap-network/thermometer/sensor/prediction.c
@@ -1,9 +1,21 @@
-// sensor/prediction.c
-//#include "temperature_forecast.h" // emlearn model
 #include "prediction.h"
+#include "temperature_forecast_converted.h"
+#include "eml_trees.h"
+#include "buffer.h"
+#include <stdint.h>
 
-float predict_temperature() {
-    return 1;
-    // float *input = get_buffer();
-    // return eml_trees_regress1(&temperature_forecast_converted, input, BUFFER_SIZE);
+float predict_temperature(void) {
+    float *buffer = get_buffer();
+    int start = get_buffer_start_index();
+    int32_t input_fixed[BUFFER_SIZE];
+
+    for (int i = 0; i < BUFFER_SIZE; i++) {
+        int index = (start + i) % BUFFER_SIZE;
+        input_fixed[i] = (int32_t)((buffer[index] - MIN_INPUT) * SCALE);
+    }
+
+    int32_t prediction = eml_trees_regress(&temperature_forecast, input_fixed, BUFFER_SIZE);
+    float output = ((float)prediction / SCALE);
+
+    return output;
 }
--- coap-network/thermometer/sensor/prediction.h
+++ coap-network/thermometer/sensor/prediction.h
@@ -1,15 +1,9 @@
 #ifndef PREDICTION_H
 #define PREDICTION_H
 
+float predict_temperature(void);
 
-#include "config.h"
-#include "buffer.h"
+#define SCALE 10       
+#define MIN_INPUT 0    
 
-void sensor_on(void);
-void sensor_off(void);
-int sensor_is_active(void);
-float generate_random_temperature(void);
-void update_buffer(float value);
-int buffer_is_full(void);
-
-#endif /* PREDICTION_H */
+#endif // PREDICTION_H
