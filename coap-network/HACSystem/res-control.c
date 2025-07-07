d6ba54c1f58043e0515efa074acd1fe7ecf94df8
msegreto <msegreto1202@gmail.com>
2025-07-07 10:50:43 +0200

    some adjustment on on/off system

--- coap-network/HACSystem/res-control.c
+++ coap-network/HACSystem/res-control.c
@@ -88,20 +88,35 @@ static void res_get_handler(coap_message_t *request, coap_message_t *response, u
 
 // === ON / OFF COAP HANDLERS ===
 
-static void res_get_on(coap_message_t *request, coap_message_t *response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset) {
+static void res_post_on(coap_message_t *request, coap_message_t *response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset) {
   sensor_on();
   const char *msg = "Actuator ON";
   coap_set_payload(response, (uint8_t *)msg, strlen(msg));
 }
 
-static void res_get_off(coap_message_t *request, coap_message_t *response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset) {
+static void res_post_off(coap_message_t *request, coap_message_t *response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset) {
   sensor_off();
   const char *msg = "Actuator OFF";
   coap_set_payload(response, (uint8_t *)msg, strlen(msg));
 }
 
-RESOURCE(res_on, "title=\"Actuator ON\"", res_get_on, NULL, NULL, NULL);
-RESOURCE(res_off, "title=\"Actuator OFF\"", res_get_off, NULL, NULL, NULL);
+static void res_get(coap_message_t *request, coap_message_t *response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset) {
+  if(is_on) {
+    LOG_INFO("[ActuatorCtrl] Actuator is  ON\n");
+    const char *msg = "Actuator is  ON";
+    coap_set_payload(response, (uint8_t *)msg, strlen(msg));
+    return;
+  }
+  else {
+    LOG_INFO("[ActuatorCtrl] Actuator is  OFF\n");
+    const char *msg = "Actuator is  OFF";
+    coap_set_payload(response, (uint8_t *)msg, strlen(msg));
+    return;
+  }
+}
+
+RESOURCE(res_on, "title=\"Actuator ON\"", res_get, res_post_on, NULL, NULL);
+RESOURCE(res_off, "title=\"Actuator OFF\"", res_get, res_post_off, NULL, NULL);
 
 // === SENSOR CONTROL FUNCTIONS ===
 
