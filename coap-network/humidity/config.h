778ad30a672161b1decc33ce3b3b870197f60c8d
msegreto <msegreto1202@gmail.com>
2025-07-11 08:38:43 +0200

    extended forecast window

--- coap-network/humidity/config.h
+++ coap-network/humidity/config.h
@@ -1,11 +1,11 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-#define SENSING_PERIOD_SECONDS 5
+#define SENSING_PERIOD_SECONDS 60*30 // 30 minutes
 #define MEAN_HUMIDITY 41.1
 #define STD_HUMIDITY 5.83
 #define MAX_STEP 2.00
-#define BUFFER_SIZE 10
+#define BUFFER_SIZE 24 // the model use the 12h before for next 30 minutes forecasting 
 
 #define CLOUD_SERVER_EP "coap://[fd00::1]:5683"
 #define REGISTRATION_RESOURCE_PATH "registration"
--- coap-network/thermometer/config.h
+++ coap-network/thermometer/config.h
@@ -1,11 +1,11 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-#define SENSING_PERIOD_SECONDS 5
+#define SENSING_PERIOD_SECONDS 60*30 // 30 minutes
 #define MEAN_TEMPERATURE 26.34
 #define STD_TEMPERATURE 1.85
 #define MAX_STEP 0.50
-#define BUFFER_SIZE 60
+#define BUFFER_SIZE 24 // the model use the 12h before for next 30 minutes forecasting 
 
 #define CLOUD_SERVER_EP "coap://[fd00::1]:5683"
 #define REGISTRATION_RESOURCE_PATH "registration"
